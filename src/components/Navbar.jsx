10e9e3e2c4951a70a46be543373e6f56abb3018e
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2024-07-04 12:35:56 +0530

    Latest

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div>
+      <header>
+        <h1>This is a Navbar</h1>
+      </header>
+      <main>
+        <h2>This is a main</h2>
+      </main>
+    </div>
+  );
+}
+
+export default Navbar;
